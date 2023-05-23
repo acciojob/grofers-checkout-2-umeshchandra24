d8c515d34e008ee1a5ad22d5fc8df7a4f6afaec4
umeshchandra24 <120272310+umeshchandra24@users.noreply.github.com>
2023-05-23 07:09:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 //your code here
-let value = document.querySelectorAll('[data-ns-test="prices"]')
-let totalValue = 0;
-value.forEach(ele =>{
-    totalValue += parseFloat(ele.textContent);
-})
+// Get the prices elements
+const pricesElements = document.querySelectorAll('[data-ns-test="prices"]');
 
-const grandTotal = document.querySelector('[data-ns-test="grandTotal"]');
-grandTotal.textContent = totalValue.toFixed(2);
+// Calculate the total price
+let totalPrice = 0;
+pricesElements.forEach(element => {
+  const price = parseFloat(element.textContent);
+  if (!isNaN(price)) {
+    totalPrice += price;
+  }
+});
+
+// Display the total price
+const grandTotalElement = document.querySelector('[data-ns-test="grandTotal"]');
+grandTotalElement.textContent = totalPrice.toFixed(2);
+
+// Assertion check
+const assert = (condition, message) => {
+  if (!condition) {
+    throw new Error(message);
+  }
+};
+
+// Verify if the calculated total matches the grand total displayed
+const displayedTotal = parseFloat(grandTotalElement.textContent);
+assert(totalPrice === displayedTotal, "Total price mismatch!");
+
+console.log("Total price calculation and grand total match.");
