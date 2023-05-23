3d749927f4f624cdba5a7fb1845309161a540342
umeshchandra24 <120272310+umeshchandra24@users.noreply.github.com>
2023-05-23 06:58:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+let value = document.querySelectorAll('[data-ns-test="prices"]')
+let totalValue = 0;
+value.forEach(ele =>{
+    totalValue += parseFloat(ele.textContent);
+})
+
+const grandTotal = document.querySelector('[data-ns-test="grandTotal"]');
+grandTotal.textContent = totalValue.toFixed(2);
